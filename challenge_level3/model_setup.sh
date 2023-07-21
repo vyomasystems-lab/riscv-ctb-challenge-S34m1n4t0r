f6841b93a497b7dca712da680b30d993287e5dcc
S34m1n4t0r <81908108+S34m1n4t0r@users.noreply.github.com>
2023-07-21 09:50:13 +0400

    Test directed_test with own core

--- challenge_level3/model_setup.sh
+++ challenge_level3/model_setup.sh
@@ -1,4 +1,4 @@
-export BLUESPECDIR=$(dirname $(which bsc))/../lib
-export PATH=/tools/mod_spike/bin:$PATH
+#export BLUESPECDIR=$(dirname $(which bsc))/../lib
+#export PATH=/tools/mod_spike/bin:$PATH
 
-git clone https://gitlab.com/S34m1n4t0r/planigale_riscv
+git clone -b feature/aapg_support https://gitlab.com/S34m1n4t0r/planigale_riscv
