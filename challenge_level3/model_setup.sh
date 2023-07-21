94561f67929850a7a01ca2a23f920eb17c89860b
Sean <schafseandalton@gmail.com>
2023-07-21 10:16:31 +0200

    Save progressm 100 random instr

--- challenge_level3/model_setup.sh
+++ challenge_level3/model_setup.sh
@@ -1,4 +1,4 @@
 #export BLUESPECDIR=$(dirname $(which bsc))/../lib
 #export PATH=/tools/mod_spike/bin:$PATH
 
-git clone -B ctb_2023 https://gitlab.com/S34m1n4t0r/planigale_riscv
+git clone -b ctb_2023 https://gitlab.com/S34m1n4t0r/planigale_riscv
